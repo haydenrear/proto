3a5479516e63d9dd4d767746ddc0cb7c15f71875
hayden.rear <hayden.rear@gmail.com>
2024-08-07 18:05:48 -0400

    updated

---
+++ src/test/java/com/hayden/proto/ProtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hayden.proto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

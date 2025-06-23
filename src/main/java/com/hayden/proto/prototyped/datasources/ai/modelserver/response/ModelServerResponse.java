c7cb6bcb835d27caa0975ca9ff79656289edcfd7
hayden.rear <hayden.rear@gmail.com>
2025-06-23 08:37:56 -0400

    Updated to make it faster... also added potential structured concurrency outling for later in comment...

--- src/main/java/com/hayden/proto/prototyped/datasources/ai/modelserver/response/ModelServerResponse.java
+++ src/main/java/com/hayden/proto/prototyped/datasources/ai/modelserver/response/ModelServerResponse.java
@@ -1,5 +1,6 @@
 package com.hayden.proto.prototyped.datasources.ai.modelserver.response;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.hayden.proto.prototyped.datasources.ai.modelserver.client.ModelServerCodingAiClient;
@@ -33,6 +34,12 @@ public sealed interface ModelServerResponse
     record ModelServerCodeResponse<T>(ModelServerCodingAiClient.CodeResult<T> codeResult)
             implements ModelServerResponse {}
 
-    record ToolCalls(List<AssistantMessage.ToolCall> codeResult){}
+    record ToolCalls(List<AssistantMessage.ToolCall> codeResult, float version) {
+
+        @JsonCreator
+        public ToolCalls(List<AssistantMessage.ToolCall> codeResult) {
+            this(codeResult, 1.0f);
+        }
+    }
 
 }
